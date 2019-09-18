b1b340c8627d7119ffade879da472e075191d639
Mark Mauro <markymauro@gmail.com>
2019-09-18 11:47:35 -0400

    Add files via upload

---
+++ 9-18-19/equation.py
@@ -0,0 +1,9 @@
+x=2
+y=2
+a=2
+b=2
+c=2
+
+equation = (3+4*x)/5 - (10*(y-5)*(a+b+c)) / x + (9)*((4/x) + ((9+x)/y))
+
+print(equation)
---
+++ 9-18-19/radius.py
@@ -0,0 +1,3 @@
+radius = 1.0
+area = radius * radius * 3.14159
+print("The radius is", radius, "The area of the circle is", area)
