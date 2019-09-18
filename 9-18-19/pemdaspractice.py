c178085125b3af2a2295c9ae5657ad09f4593ff6
Mark Mauro <markymauro@gmail.com>
2019-09-18 12:04:18 -0400

    Add files via upload

---
+++ 9-18-19/pemdaspractice.py
@@ -0,0 +1,7 @@
+print(2*(5//2 + 5//2)) # 5//2 truncates to 2, therefore, (2 + 2) * 2 = answer is 8
+
+print(2 * 5 // 2 + 2 * 5 //2)   # 10 // 2  + 10 // 2 = 5+5 = answer is 10
+
+print(2 * (5//2)) #2 * (2) = answer is 4
+
+print(2 * 5 // 2) # 2 * 5, then integer divide by two. answere is 5
