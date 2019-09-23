f61ec096336a33e03981a8f2ba7df53a8eee8f68
Mark Mauro <markymauro@gmail.com>
2019-09-23 12:45:07 -0400

    Add files via upload

---
+++ 9-23-19/nested.py
@@ -0,0 +1,11 @@
+i = 12
+k = 10
+j = 5
+
+if i > k:
+    if j > k:
+        print("i and j are greater than k")
+    else:
+        print("k is greater than or equal to j")
+else:
+    print("i is less than or equal to k")
