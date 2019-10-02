17d4b668a67d270c411b1ab4419ebaa9de8d3d1d
Mark Mauro <markymauro@gmail.com>
2019-10-02 12:16:30 -0400

    Add files via upload

---
+++ 10-2-19/between11and50.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: " ))
+
+if(num >= 0 and num <= 10):
+    print("Between 0 and 10")
+elif(num > 10 and num <= 50):
+    print("Between 11 and 50")
+else:
+    print("Greater than 50")
