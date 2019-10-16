0017f2921d383970c14d0f9322bcd2c303fbd2f3
Mark Mauro <markymauro@gmail.com>
2019-10-16 12:22:02 -0400

    Add files via upload

---
+++ 10-16-19/evenIndex.py
@@ -0,0 +1,5 @@
+x = str(input("Enter a string: "))
+
+s = x[0:len(x):2]
+
+print(s)
---
+++ 10-16-19/yes.py
@@ -0,0 +1,5 @@
+x = int(input("Enter an integer: "))
+
+print("Hi Five Georgia" if (age % 5 == 0) and (age % 3 == 0))
+print("Hi Five" if age % 5 == 0)
+print("Georgia" if age % 3== 0)
