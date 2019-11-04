a4506f5aceb39f6867b9cd5f063d92efee8b6766
Mark Mauro <markymauro@gmail.com>
2019-11-04 12:29:22 -0500

    Add files via upload

---
+++ 11-4-19/list3.py
@@ -0,0 +1,20 @@
+list1 = list("ABCD")
+list2 = [100,12,1,0,134]
+
+print(list1[0])
+print(list2[1])
+
+list3 =  list1 + list2
+
+print(list3)
+
+list4 = [2,3,2,3]
+
+list4 = list4[2:4]
+print(list4)
+
+
+list5 = [2,3,5,2,33,21]
+print(list5[-1])
+
+print(2 in list5)
