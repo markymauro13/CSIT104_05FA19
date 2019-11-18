a02eeb4219c5767d4e034fd6ea9037987de62556
Mark Mauro <markymauro@gmail.com>
2019-11-18 18:57:33 -0500

    Update list10.py

--- 11-18-19/list10.py
+++ 11-18-19/list10.py
@@ -2,4 +2,6 @@ list1 = [x for x in range(0,5)] # returns a list of 0,1,2,4
 print(list1)
 
 # write a statement to print even numbers 1-100
+evenList = [x for x in range(2,101,2)]
+print(evenList)
 
