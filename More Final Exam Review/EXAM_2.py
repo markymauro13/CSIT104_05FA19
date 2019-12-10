f7703f0c185083d89bc6900f06e2c20c75dcbc99
Mark Mauro <markymauro@gmail.com>
2019-12-10 18:46:20 -0500

    Create EXAM_2.py

---
+++ More Final Exam Review/EXAM_2.py
@@ -0,0 +1,9 @@
+#1, write a program that will print only the odd index characters in a string starting from 0 index
+
+x = input("Enter a string")
+
+for i in range(0,len(x)):
+     if i % 2 == 0:
+         print(x[i], end='')
+
+#2, What will be the output of the following code?
