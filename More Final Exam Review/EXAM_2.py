f2f460469df9fdd9bbc5ee774d63cffdb178d645
Mark Mauro <markymauro@gmail.com>
2019-12-10 22:04:33 -0500

    Update EXAM_2.py

--- More Final Exam Review/EXAM_2.py
+++ More Final Exam Review/EXAM_2.py
@@ -70,3 +70,51 @@ while i >= 1:
           print(j, end = '')
      print()
      i-=1
+     
+#5 What will be the output of the following program
+
+#a
+
+balance = 1000
+while True:
+     if balance < 9:
+          break
+     balance = balance - 9
+print("Balance is", balance)
+
+#b, Suppose a function is as follows:
+
+def f(p1,p2=5,p3=5,p4=1):
+     return p1+p2+p3+p4
+print(f(1,p2=3,p3=4,p4=4))    #This is correct, OUTPUT = 12
+print(f(1,p2=3,4,p4=4))       #This is incorrect, will result in error
+print(f(p1=1,p2=3,4,p4=4))    #This is incorrect, will result in error
+print(f(1,4,4))               #This is correct, OUTPUT = 10
+print(f(p4=1,p2=3,p3=4,p1=4)) #This is correct, OUTPUT = 12
+
+#c, What will be the output of the following code
+
+def m(n):
+     n+=1
+def main():
+     n = 1
+     m(n)
+     print(n)
+main()
+
+#6 Write a python program that will print the reverse of a string. For example, if the given string is "python", it will print "nohtyp"
+
+x = input("Enter a string:")
+print(x[::-1]) # reverses string
+
+#7 Write a python function that will take a string as a parameter and then return that string in uppercase. For example if the given parameter
+# is "msu" it will return "MSU" which will be printed by the caller
+
+def toUpper(str):
+     return str.upper()
+print(toUpper("msu"))    # will return "MSU"
+
+
+
+
+
